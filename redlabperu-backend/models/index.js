bf4a193fc3c35717f77e28d5e340ae368aa73112
tefa0408 <estefanyalfaro2230@gmail.com>
2022-05-20 15:29:42 -0500

    .

--- redlabperu-backend/models/index.js
+++ redlabperu-backend/models/index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
@@ -16,7 +16,7 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config, config.dialect);
 }
 
 fs
