c7f2769e1f09abce883e9b258fa864a15c6086db
tefa0408 <estefanyalfaro2230@gmail.com>
2022-06-01 08:02:30 -0500

    config

--- redlabperu-backend/config.js
+++ redlabperu-backend/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: process.env.PORT || 5000,
   db: process.env.DB || "dcr9b841s6g3dp",
-  url: process.env.URL || "localhost",
+  url: process.env.URL || "https://redlab-peru.herokuapp.com/",
   urlFront: process.env.URL_FRONT || "localhost",
   portFront: process.env.PORT_FRONT || 3000,
   secret_key: process.env.SECRET_KEY || "712386210123",
